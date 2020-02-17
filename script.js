09ee364008829b4d03c169567e3c9e0f79e57fdc
fionawray <fion0659@stud.kea.dk>
2020-02-17 10:58:09 +0100

    console

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+sayHello("Fiona");
+
+function presentPet(firstName, animalType, animalName) {
+  console.log(
+    `My name is ${firstName} i have a ${animalType} called ${animalName}`
+  );
+}
+
+function sayHello(firstName) {
+  console.log(`Hello ${firstName}`);
+}
